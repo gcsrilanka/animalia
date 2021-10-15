001c2cad552f31abe05266dc1e325b4bb156bc14
sanketh <kinisanketh@gmail.com>
2021-10-15 11:51:09 +0530

    Revert "login api added"
    This reverts commit 772f79a849f567281a53769e9d24c6eb8658d96a.

--- app.js
+++ app.js
@@ -1,45 +1,13 @@
 const express = require("express");
-const cors=require('cors');
-const dotenv=require('dotenv').config();
-const HttpError=require('./Model/HttpError');
-const app = express();
-const mongoose=require('mongoose');
 
-//cors
-app.use(cors());
+const app = express();
 
 // Instead of using body-parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-//db connection
-mongoose.connect(process.env.db_uri)
-const db=mongoose.connection;
-db.on('open',()=>{
-  console.log('database connected');
-})
-
-db.on('error',()=>{
-  console.log('error while connecting database');
-})
-
-//api routes
-const route=require('./Routes/userRoutes');
-app.use("/api",route);
-
-//exception for wrong requests
-app.use((req,res,next)=>{
-  var error=new HttpError('not found',404);
-  next(error);
-})
-
-//response with error message and code
-app.use((error,req,res,next)=>{
-  return res.status(error.code).json({message:error.message});
-})
-
-//port listing on 3000
-app.listen(3000,()=>{
-  console.log("listing")
+app.use("/", (req, res) => {
+  res.send("it works");
 });
+
+app.listen(3000);
